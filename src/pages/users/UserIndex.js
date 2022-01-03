384e635782d3465cd429082a89111d5442f22c10
cebasypia.dev01 <cebasypia.tskm8+dev01@gmail.com>
2022-01-03 21:48:30 +0900

    Add GET /users

--- src/pages/users/UserIndex.js
+++ src/pages/users/UserIndex.js
@@ -1,12 +1,29 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const UserIndex = () => {
+  const [users, setUsers] = useState([]);
+  const APP_URL = `http://localhost:8000/users`;
+
+  useEffect(() => {
+    fetch(APP_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  }, [APP_URL]);
+
   return (
     <div>
       <h1>UserIndex</h1>
       <li>
         <Link to="123">UserProfile</Link>
       </li>
+      {users.map((user) => (
+        <ol key={user.id}>
+          <Link to={user.id}>{user.name}</Link>
+        </ol>
+      ))}
     </div>
   );
 };
